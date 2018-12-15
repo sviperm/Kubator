9a4159e3d4ac2884730addc7d3b0917f12ec4f1e
Eldar Khalitov <eldar.khalitov.cybermd@gmail.com>
2018-12-15 17:39:21 +0300

    added roles.py

---
+++ Kubator/roles.py
@@ -0,0 +1,20 @@
+from rolepermissions.roles import AbstractUserRole
+
+
+class MedWorker(AbstractUserRole):
+    available_permissions = {
+
+    }
+
+
+class Patient(AbstractUserRole):
+    available_permissions = {
+
+    }
+
+
+class Manager(AbstractUserRole):
+    available_permissions = {
+
+    }
+
