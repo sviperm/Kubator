eb8afc6dd44852ba0711a91f40f889058d0af19b
Eldar Khalitov <eldar.khalitov.cybermd@gmail.com>
2018-12-17 12:55:51 +0300

    added patient model

--- patient/models.py
+++ patient/models.py
@@ -1,7 +1,19 @@
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Patient(models.Model):
+class Address(models.Model):
     pass
+
+
+class PatientProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    middle_name = models.CharField(max_length=150, verbose_name="Фамилия")
+    data_of_birth = models.DateField(verbose_name="Дата рождения")
+    address = models.ForeignKey(Address, on_delete=models.DO_NOTHING)
+
+    # https://ru.wikipedia.org/wiki/Телефонный_номер
+    # 15 знаков на цифры и "+" - префикс
+    phone_number = models.CharField(max_length=16, verbose_name="Телефонный номер")
+
