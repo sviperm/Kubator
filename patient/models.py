a86c192ba01822736e83d3f052d2a2a14501c891
Eldar Khalitov <eldar.khalitov.cybermd@gmail.com>
2018-12-17 14:29:12 +0300

    changed adrress field to TextField

--- patient/models.py
+++ patient/models.py
@@ -3,15 +3,16 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
-class Address(models.Model):
-    pass
+# class Address(models.Model):
+#     pass
 
 
 class PatientProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     middle_name = models.CharField(max_length=150, verbose_name="Фамилия")
     data_of_birth = models.DateField(verbose_name="Дата рождения")
-    address = models.ForeignKey(Address, on_delete=models.DO_NOTHING)
+    # address = models.ForeignKey(Address, on_delete=models.DO_NOTHING)
+    address = models.TextField()
 
     # https://ru.wikipedia.org/wiki/Телефонный_номер
     # 15 знаков на цифры и "+" - префикс
