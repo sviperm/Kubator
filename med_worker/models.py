71214b61625349f3a56a9c1f25f841f1a216191f
Eldar Khalitov <eldar.khalitov.cybermd@gmail.com>
2018-12-18 20:39:54 +0300

    rm migrations

--- med_worker/models.py
+++ med_worker/models.py
@@ -1,7 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Position(models.Model):
+    title = models.CharField(max_length=100, verbose_name="Должность")
+
+    def __str__(self):
+        return self.title
 
 
 class MedWorkerProfile(models.Model):
-    pass
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE)
+    middle_name = models.CharField(max_length=100, verbose_name="Отчество", blank=True, null=True)
+    position = models.ForeignKey('Position', on_delete=models.CASCADE, verbose_name='Должность')
