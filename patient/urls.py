4ba4f01b04823a2d197291299bc1509f4a1b9803
Eldar Khalitov <eldar.khalitov.cybermd@gmail.com>
2018-12-18 08:55:48 +0300

    moved to service app

--- patient/urls.py
+++ patient/urls.py
@@ -1,11 +1,6 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('services/', views.get_service_list),
-    path('escort/', views.escort),
-    path('child/', views.care_child),
-    path('food/', views.get_food),
-    path('fun/', views.fun),
-
-]
+# from django.urls import path
+# from . import views
+#
+# urlpatterns = [
+#
+# ]
