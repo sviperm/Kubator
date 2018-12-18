383afb784137425c5245f17e70cf3fe519cdb210
Eldar Khalitov <eldar.khalitov.cybermd@gmail.com>
2018-12-18 20:32:06 +0300

    copied from vlad_dev

--- manager/models.py
+++ manager/models.py
@@ -1,7 +1,9 @@
 from django.db import models
-# from django.contrib.auth.models import User
+from django.contrib.auth.models import User
 
 
 # Create your models here.
 class ManagerProfile(models.Model):
-    pass
+    user = models.OneToOneField(User,
+                                on_delete=models.CASCADE)
+    middle_name = models.CharField(max_length=100, verbose_name="Отчество")
