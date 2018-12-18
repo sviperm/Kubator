0eeffd0b07655bb1c05c3181ab63afff5d45083e
Eldar Khalitov <eldar.khalitov.cybermd@gmail.com>
2018-12-18 08:57:01 +0300

    moved to service app

--- patient/views.py
+++ patient/views.py
@@ -1,32 +1,5 @@
-from django.shortcuts import render
+# from django.shortcuts import render
 
 
 # Create your views here.
-def get_service_list(request):
-
-    return render(request, 'patient/service_list.html')
-
-
-def care_child(request):
-    pass
-
-
-def get_food(request):
-    pass
-
-
-def escort(request):
-    pass
-
-
-def fun(request):
-    pass
-
-
-def alert(request):
-    pass
-
-
-def get_archive(request):
-    pass
 
