83e553a3af60634883e6433f8bf79ce0bf685519
Vladislav Safonichev <unsviperm@gmail.com>
2018-12-24 00:46:31 +0300

    + verbose_names for patient and med_worker

--- med_worker/models.py
+++ med_worker/models.py
@@ -11,7 +11,9 @@ class Position(models.Model):
 
 class MedWorkerProfile(models.Model):
     user = models.OneToOneField(User,
-                                on_delete=models.CASCADE)
+                                on_delete=models.CASCADE,
+                                verbose_name="Пользователь")
+    raw_password = models.CharField(max_length=8, verbose_name="Пароль")
     middle_name = models.CharField(max_length=100, verbose_name="Отчество")
     position = models.ForeignKey(
         'Position',
@@ -20,4 +22,4 @@ class MedWorkerProfile(models.Model):
     )
 
     def __str__(self):
-        return f'{self.user.last_name} {self.user.first_name} {self.middle_name}: {self.position}'
+        return f'{self.user.username} {self.raw_password} {self.user.last_name} {self.user.first_name} {self.middle_name}: {self.position}'
--- patient/models.py
+++ patient/models.py
@@ -4,9 +4,14 @@ from django.contrib.auth.models import User
 
 class PatientProfile(models.Model):
     user = models.OneToOneField(User,
-                                on_delete=models.CASCADE)
+                                on_delete=models.CASCADE,
+                                verbose_name="Пользователь")
+    raw_password = models.CharField(max_length=8, verbose_name="Пароль")
     middle_name = models.CharField(max_length=100, verbose_name="Отчество")
     birth_date = models.DateField(null=False, verbose_name="Дата рождения")
     address = models.TextField(verbose_name="Адрес")
     contact_phone = models.CharField(max_length=16,
                                      verbose_name="Контактный телефон")
+
+    def __str__(self):
+        return f'{self.user.last_name} {self.user.first_name}'
