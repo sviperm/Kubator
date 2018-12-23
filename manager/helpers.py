2e6c678d3f2936c45bb09d23c88ae88cb684e6f2
Vladislav Safonichev <unsviperm@gmail.com>
2018-12-24 00:44:58 +0300

    + login and password generator for registration

--- manager/helpers.py
+++ manager/helpers.py
@@ -1,4 +1,24 @@
-# from 
+from patient.models import PatientProfile
+from med_worker.models import MedWorkerProfile
 
-# def gen_username_and_password (profile):
 
+def gen_username(profile):
+    if profile == 'patient':
+        object_num = PatientProfile.objects.count() + 1
+    elif profile == 'medworker':
+        object_num = MedWorkerProfile.objects.count() + 1
+    else:
+        return ValueError
+    return f'{profile}_{object_num}'
+
+
+def is_manager(user):
+    return user.groups.filter(name='manager').exists()
+
+
+def is_patient(user):
+    return user.groups.filter(name='patient').exists()
+
+
+def is_medworker(user):
+    return user.groups.filter(name='medworker').exists()
