3e30c4cec06a95a965f1e23d065f6d5186673df0
Eldar Khalitov <eldar.khalitov.cybermd@gmail.com>
2018-12-24 16:47:38 +0300

    fixed

--- service/admin.py
+++ service/admin.py
@@ -1,8 +1,4 @@
 from django.contrib import admin
-<<<<<<< HEAD
-from . models import Service
-=======
-
 from .models import MedWorkerProfile, Position, PatientProfile, Order, OrderStatus, Service
 
 admin.site.register(Position)
@@ -11,18 +7,3 @@ admin.site.register(PatientProfile)
 admin.site.register(Order)
 admin.site.register(OrderStatus)
 admin.site.register(Service)
-
-# from . models import Service  # ,TypeService
-
-# Register your models here.
-
-
-# @admin.register(TypeService)
-# class TypeServiceAdmin(admin.ModelAdmin):
-#     pass
->>>>>>> 3f0fe9b3ee52eb392706417b6620ea8212576836
-
-
-# @admin.register(Service)
-# class ServiceAdmin(admin.ModelAdmin):
-#     pass
