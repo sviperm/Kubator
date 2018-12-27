5b48688cb9f8031630f13dc3d723ea0d340b35aa
Vladislav Safonichev <unsviperm@gmail.com>
2018-12-28 00:00:48 +0300

    + all manager pages

--- manager/forms.py
+++ manager/forms.py
@@ -1,23 +1,29 @@
 from django import forms
-from service.models import PatientProfile, MedWorkerProfile
+from service.models import PatientProfile, MedWorkerProfile, Ward, Position
 
 
 class SignUpPatientForm(forms.ModelForm):
-    last_name = forms.CharField(label='Фамилия', max_length=100)
-    first_name = forms.CharField(label='Имя', max_length=100)
-    birth_date = forms.DateField(label='Дата рождения',
+    last_name = forms.CharField(label='', max_length=100)
+    first_name = forms.CharField(label='', max_length=100)
+    birth_date = forms.DateField(label='',
                                  widget=forms.DateInput(
                                      attrs={'type': 'date'}))
+    ward = forms.ModelChoiceField(queryset=Ward.objects.all(),
+                                  empty_label='Палата',
+                                  label='')
 
     class Meta:
         model = PatientProfile
         fields = ['last_name', 'first_name', 'middle_name',
-                  'birth_date', 'address', 'contact_phone']
+                  'birth_date', 'address', 'contact_phone', 'ward']
 
 
 class SignUpMedWorkerForm(forms.ModelForm):
-    last_name = forms.CharField(label='Фамилия', max_length=100)
-    first_name = forms.CharField(label='Имя', max_length=100)
+    last_name = forms.CharField(label='', max_length=100)
+    first_name = forms.CharField(label='', max_length=100)
+    position = forms.ModelChoiceField(queryset=Position.objects.all(),
+                                      empty_label='Должность',
+                                      label='')
 
     class Meta:
         model = MedWorkerProfile
--- manager/views.py
+++ manager/views.py
@@ -1,19 +1,30 @@
 from django.contrib.auth.decorators import login_required, user_passes_test
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User, Group
 from .forms import SignUpPatientForm, SignUpMedWorkerForm
+from .models import ManagerProfile
 from .helpers import gen_username, is_manager
-from django.http import Http404
 
 
-@user_passes_test(is_manager, redirect_field_name='')
+@user_passes_test(is_manager, redirect_field_name='', login_url='account:home')
 def index(request):
     # TODO: orders in tabs, close order
-    return render(request, 'manager/manager_cab.html')
+    if request.method == 'POST':
+        if request.POST['redirect'] == 'medworker':
+            return redirect('manager:signup', 'medworker')
+        elif request.POST['redirect'] == 'patient':
+            return redirect('manager:signup', 'patient')
+    else:
+        middle_name = ManagerProfile.objects.get(
+            user=request.user.pk).middle_name
+        context = {'last_name': request.user.last_name,
+                   'first_name': request.user.first_name,
+                   'middle_name': middle_name}
+        return render(request, 'manager/manager_cab.html', context)
 
 
-@user_passes_test(is_manager, redirect_field_name='')
+@user_passes_test(is_manager, redirect_field_name='', login_url='account:home')
 def signup(request, profile):
     if request.method == 'POST':
         if profile == 'patient':
@@ -35,17 +46,16 @@ def signup(request, profile):
             profile.user = user
             profile.raw_password = password
             profile.save()
-            # Get last created object
-            # https://docs.djangoproject.com/en/2.1/ref/models/querysets/#latest
-            return HttpResponse(f'Login: {username} Password: {password}')
+            context = {'username': username,
+                       'password': password}
+            return render(request, 'manager/login_password.html', context)
     else:
         if profile == 'patient':
             form = SignUpPatientForm()
+            return render(request, 'manager/patient_sign_up.html', {'form': form})
         elif profile == 'medworker':
             form = SignUpMedWorkerForm()
+            return render(request, 'manager/medworker_sign_up.html', {'form': form})
         else:
-            # TODO: create an HTML template named 404.html
-            # and place it in the top level of template tree.
-            # TODO: set DEBUG to False.
             raise Http404()
     return render(request, 'manager/sign_up.html', {'form': form})
